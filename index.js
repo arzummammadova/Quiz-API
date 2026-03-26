e94022a4740b453065dac267b1114ee7ed3e0e6a
Arzu Mammadova <arzuam-af106@code.edu.az>
2026-03-26 23:28:32 +0400

    first bro

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const app=express();
+const PORT=process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
